440b15c32bb09a9698fa5cac7612ae92f5efa9ac
jamesjuuichi <nguyenkhactunganh@gmail.com>
2023-04-23 17:25:02 +0800

    ! Remove unnescessary things

--- keyboards/massdrop/ctrl/keymaps/default/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/default/keymap.c
@@ -120,12 +120,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
               }
             }
             return false;
-        case KC_W:
-            if (record->event.pressed && MODS_SHIFT) {
-                
-            }
-            return false;
-
         default:
             return true; //Process all other keycodes normally
     }
